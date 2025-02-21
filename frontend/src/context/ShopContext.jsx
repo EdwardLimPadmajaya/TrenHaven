dd60ab33d05b35c567e2ec92fda37a0498fdf6f2
Edward3957503 <edwardpadmajaya@gmail.com>
2025-02-21 17:25:12 +1100

    Integrated products from backend to frontend

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,17 +1,18 @@
 import { createContext, useEffect, useState } from "react";
-import { products } from "../assets/assets";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
-
+import axios from 'axios'
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
 
     const currency = '$';
     const delivery_fee = 10;
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
+    const [products, setProducts] = useState([]);
     const navigate = useNavigate();
 
     const addToCart = async (itemId, size) => {
@@ -69,12 +70,33 @@ const ShopContextProvider = (props) => {
                         totalAmount += itemInfo.price * cartItems[items][item];
                     }
                 } catch (error) {
-
+                    console.log(error);
+                    toast.error(error.message);
                 }
             }
         }
         return totalAmount;
     }
+
+    const getProductsData = async () => {
+        try {
+            const response = await axios.get(backendUrl + '/api/product/list');
+            if(response.data.success) {
+                setProducts(response.data.products);
+            }
+            else {
+                toast.error(response.data.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message);
+        }
+    }
+
+    useEffect(() => {
+        getProductsData();
+    }, []);
+
     const value = {
         products,
         currency,
@@ -88,7 +110,8 @@ const ShopContextProvider = (props) => {
         getCartCount,
         updateQuantity,
         getCartAmount,
-        navigate
+        navigate,
+        backendUrl
     }
 
     return (
