528026f444cd373f6e252fb080c99557b5f04b5b
Edward3957503 <edwardpadmajaya@gmail.com>
2025-02-23 18:00:05 +1100

    Integrated orders page from backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -44,7 +44,14 @@ const allOrders = async(req, res) => {
 
 // All Orders data for frontend
 const userOrders = async(req, res) => {
-    
+    try {
+        const { userId } = req.body;
+        const orders = await orderModel.find({ userId });
+        res.json({success: true, orders});
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message});
+    }
 }
 
 // Update order status from Admin
